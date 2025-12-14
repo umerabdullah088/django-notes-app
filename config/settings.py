0c3c9aaaeaac7749d9dd550d3ec6fb35e0c8a81f
UMER ABDULLAH <umerabdullah@SHEIKHS-macbook.local>
2025-12-14 18:16:16 +0530

    Read SECRET_KEY from environment

--- config/settings.py
+++ config/settings.py
@@ -10,8 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!gr2-)xx2o(m=ov^f@0e5=%n9u+#ldsss0wa^yv&-w$#wp67oc'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
